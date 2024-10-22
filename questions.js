8db69d972a518885268a13cdcb13c66b000b9a0f
Rachgit28 <119473857+Rachgit28@users.noreply.github.com>
2024-10-22 20:59:03 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stands for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. CodeHal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for?",
+        answer: "A. HyperText Preprocessor",
+        options: [
+            "A. HyperText Preprocessor",
+            "B. HomeText Programming",
+            "C. HyperText Programming",
+            "D. Programming HyperText Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stands for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Style Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stands for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language",
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does LIFO stands for?",
+        answer: "D. Last In First Out",
+        options: [
+            "A. Last Inside First Outside",
+            "B. Last In Fish Out",
+            "C. Learn In Finish Out",
+            "D. Last In First Out",
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does LIFO stands for?",
+        answer: "D. Last In First Out",
+        options: [
+            "A. Last Inside First Outside",
+            "B. Last In Fish Out",
+            "C. Learn In Finish Out",
+            "D. Last In First Out",
+        ]
+    }
+]
